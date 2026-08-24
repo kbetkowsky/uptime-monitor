215f8bb43bd94b4e327bed11012ffa993ef64bdc
kbetkowsky <k.betkowsky@gmail.com>
2026-08-24 17:01:22 +0200

    refactor: move enable annotations to config classes

--- src/main/java/com/kbetkowski/uptimemonitor/UptimeMonitorApplication.java
+++ src/main/java/com/kbetkowski/uptimemonitor/UptimeMonitorApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableCaching
 public class UptimeMonitorApplication {
 
 	public static void main(String[] args) {
