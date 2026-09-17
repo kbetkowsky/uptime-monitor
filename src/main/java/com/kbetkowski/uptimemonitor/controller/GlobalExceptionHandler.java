9b8d85631e069651841c224188c1a6bf9d387f82
kbetkowsky <k.betkowsky@gmail.com>
2026-09-17 19:31:04 +0200

    fix: return field errors when request body is invalid

--- src/main/java/com/kbetkowski/uptimemonitor/controller/GlobalExceptionHandler.java
+++ src/main/java/com/kbetkowski/uptimemonitor/controller/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.kbetkowski.uptimemonitor.controller;
 import com.kbetkowski.uptimemonitor.service.exception.SiteNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +27,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SiteNotFoundException.class)
     public ResponseEntity<String> handleSiteNotFound(SiteNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
